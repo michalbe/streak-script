6403a952124fd093762c3574d8a29ba449578e4f
Michał Budzyński <michal@virtualdesign.pl>
2019-01-23 23:16:02 +0100

    calculate total time

--- book-streak.1h.sh
+++ book-streak.1h.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-echo "📗 2 🕐 17:15"
+source ~/.book-streak-config
+
+total_minutes=0
+
+while read -r line
+do
+    field1=$(echo $line | awk -F',' '{printf "%s", $1}')
+    field2=$(echo $line | awk -F',' '{printf "%s", $2}')
+    let "total_minutes += field2"
+
+done < $FILE
+
+hours=$(bc <<< "$total_minutes/60")
+minutes=$(bc <<< "$total_minutes%60")
+
+printf "📗 2  🕐 %02dm%02dm\n" $hours $minutes
